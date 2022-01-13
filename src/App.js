d4db08a91e266c493a3b40eebf54e5af3af3dc9f
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-01-13 23:58:34 +0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from "./components/Sidebar/Sidebar";
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = [
+        {argument: 1, value: 10},
+        {argument: 2, value: 20},
+        {argument: 3, value: 30},
+    ];
+    return (
+        <div> Hello </div>
+
+    );
 }
 
 export default App;
